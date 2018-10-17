b87ba2294623409e6c15ac9f05671fc4b61e73b7
Steven Ennis <Steven.otieno1@gmail.com>
2018-10-17 11:15:20 +0300

    [#161252978] Add Auth routes to endpoints

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,4 +1,17 @@
 from flask import Blueprint
+from flask_restful import Api
+
 # Register blueprint application
-version_1 = Blueprint('api', __name__, url_prefix=".api/v1")
-# api = Api(version_1)
+
+from app.api.v1.views.products import Product, ProductList
+from app.api.v1.views.sales import Sale, SaleList
+
+
+version_1 = Blueprint('api', __name__, url_prefix="/api/v1")
+api = Api(version_1)
+
+api.add_resource(ProductList, '/product')
+api.add_resource(Product, '/products/<int:prod_id>')
+
+api.add_resource(SaleList, '/sale')
+api.add_resource(Sale, '/sales/<int:sale_id>')
