91717141bcd6cdc538e9de132d3a7f105e7d4ca2
Steven Ennis <Steven.otieno1@gmail.com>
2018-10-17 11:23:54 +0300

    [Finishes #161252978] No new changes made to files

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from Instance.config import app_config
 
-# creating the appa
+# creating the app
+
+
 def create_app(config_name='development'):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
 
-
+  
 # Use application to name our blueprint
     from app.api.v1 import version_1 as v1
     app.register_blueprint(v1)
